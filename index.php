e24596bde858e26165d2b73e058e33fc6cb893b7
Francescorussoo <164784843+Francescorussoo@users.noreply.github.com>
2024-10-15 13:21:18 +0100

    correzione

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             public $nome;
             public $icona;
 
-            public function __construct($nome, $icona) {
+            public function __construct($nome, $icona = '') {
                 $this->nome = $nome;
                 $this->icona = $icona;
             }
