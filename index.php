ec9a4e4ad4b2d705184849ec4d3fd4d343c3eb6a
Francescorussoo <164784843+Francescorussoo@users.noreply.github.com>
2024-10-15 13:07:35 +0100

    finito

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-commerce Pet</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <h1>Prodotti per Animali</h1>
@@ -34,6 +35,38 @@
                 echo "</div>";
             }
         }
+        class Categoria {
+            public $nome;
+            public $icona;
+
+            public function __construct($nome, $icona) {
+                $this->nome = $nome;
+                $this->icona = $icona;
+            }
+        }
+
+        class TipoProdotto {
+            public $nome;
+
+            public function __construct($nome) {
+                $this->nome = $nome;
+            }
+        }
+
+        $categoriaCani = new Categoria("Cani");
+        $categoriaGatti = new Categoria("Gatti");
+
+        $tipoCibo = new TipoProdotto("Cibo");
+        $tipoGioco = new TipoProdotto("Gioco");
+        $tipoAccessorio = new TipoProdotto("Accessorio");
+
+        $prodotto1 = new Prodotto("Shampoo per cani", 9.99, "./img/img_shampoo.jpg", $categoriaCani->nome, $tipoAccessorio->nome);
+        $prodotto2 = new Prodotto("Lettiera per gatti", 14.99, "./img/lettiera_gatti.jpg", $categoriaGatti->nome, $tipoAccessorio->nome);
+        $prodotto3 = new Prodotto("Osso per cani", 6.50, "./img/img_osso.jpg", $categoriaCani->nome, $tipoGioco->nome);
+
+        $prodotto1->mostraProdotto();
+        $prodotto2->mostraProdotto();
+        $prodotto3->mostraProdotto();
         ?>
     </div>
 </body>
